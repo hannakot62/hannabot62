92b7f14cf7991faa365fc613e12d62473d0b1c9b
HANNAKOT62 <2kotyagova6@gmail.com>
2023-06-06 00:50:29 +0300

    fixed pictures bug

--- requests/pictureRequest.js
+++ requests/pictureRequest.js
@@ -4,7 +4,7 @@ export const pictureRequest = async searchQuery => {
     let pictureURL = ''
     await axios
         .get(
-            `https://api.pexels.com/v1/search?query=${searchQuery}&per_page=1`,
+            `https://api.pexels.com/v1/search?query=${searchQuery}&per_page=80`,
             {
                 headers: {
                     Authorization: process.env.PICTURES_ACCESS_TOKEN
@@ -14,7 +14,8 @@ export const pictureRequest = async searchQuery => {
         .then(function (response) {
             // обработка успешного запроса
             console.log(response.data)
-            pictureURL = response.data.photos[0].url
+            const rand = Math.floor(Math.random() * 80)
+            pictureURL = response.data.photos[rand].url
         })
     return pictureURL
 }
