49f131a03ac53910303bf996d43b06b56649874e
HANNAKOT62 <2kotyagova6@gmail.com>
2023-06-22 05:38:30 +0300

    webpack config 5.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
 // webpack.config.js
-import path from 'path'
-import { fileURLToPath } from 'url'
+const path = require('path')
 
-const __filename = fileURLToPath(import.meta.url)
-
-const __dirname = path.dirname(__filename)
-
-export default {
-    // target: 'node',
+module.exports = {
+    target: 'node',
     entry: './index.js',
     output: {
         filename: 'bundle.js',
@@ -16,7 +11,7 @@ export default {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.cjs$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             }
