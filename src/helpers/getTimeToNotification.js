7f32f84d4640d59f565b398ecc53c189c6c2e238
HANNAKOT62 <2kotyagova6@gmail.com>
2023-06-22 14:50:21 +0300

    fixed notification timezone bug

--- src/helpers/getTimeToNotification.js
+++ src/helpers/getTimeToNotification.js
@@ -4,8 +4,10 @@ export function getTimeToNotification(hours, minutes) {
     let ms = 0
     const now = new Date()
     let notifyMoment = new Date()
+    const timezoneUTCDifference =
+        now.getTime() - now.getTimezoneOffset() * 60 * 1000
     notifyMoment.setHours(hours, minutes, 0)
-
+    notifyMoment = new Date(notifyMoment.getTime() - timezoneUTCDifference)
     if (notifyMoment.getTime() < now.getTime())
         notifyMoment = new Date(notifyMoment.getTime() + DAY_IN_MS)
 
